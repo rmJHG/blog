cf3e8654347fda611038fbea39f5386945bdb683
rmJHG <jhg990508@gmail.com>
2023-03-20 16:39:17 +0900

    baseName

--- blog/src/App.js
+++ blog/src/App.js
@@ -8,7 +8,7 @@ import Layout from "./component/layout/Layout";
 function App() {
   return (
     <Layout>
-      <Routes baseName={"/blog"}>
+      <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/user" element={<Login />} />
       </Routes>
--- blog/src/index.js
+++ blog/src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <UserLoginInfoProvider>
     <EditContextProvider>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <App />
       </BrowserRouter>
     </EditContextProvider>
